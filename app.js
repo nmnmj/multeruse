5e6ec293eaac679228c7b1be52995a21a30e4026
nmnmj <nmnjay@gmail.com>
2023-05-15 11:23:58 +0530

    r

--- app.js
+++ app.js
@@ -3,15 +3,15 @@ dotenv.config();
 import cors from "cors";
 import express, { urlencoded } from "express";
 import connectDB from "./db/connectDB.js";
-import web from './routes/web.js'
+import web from "./routes/web.js";
 import upload from "./middlewares/uploadm.js";
 const app = express();
 const port = process.env.PORT;
 const DATABASE_URL = process.env.DATABASE_URL;
 app.use(cors());
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static("public/uploads/pimage"))
+app.use(express.static("public/uploads/pimage"));
 //for form data
 app.use(express.json());
 //for form file at application level
@@ -21,8 +21,7 @@ app.use(express.json());
 // ]))
 connectDB(DATABASE_URL);
 
-app.use("/", web)
-
+app.use("/", web);
 
 app.listen(port, () => {
   console.log(`running on http://127.0.0.1:${port}`);
--- controller/formcontroller.js
+++ controller/formcontroller.js
@@ -1,32 +1,36 @@
 import applicantModel from "../models/applicant.js";
 
-class formcontroller{
+class formcontroller {
+  static home = async (req, res) => {
+    var r = 0;
+    const all = await applicantModel.find();
+    // console.log(all)
+    // res.render("home.ejs", { r, all });
+    res.send({r, all});
+  };
 
-    static home= async (req, res)=>{
-        var r =0
-        const all = await applicantModel.find()
-        // console.log(all)
-        res.render("home.ejs",{r, all})
+  static saveProfile = async (req, res) => {
+    const { name, email } = req.body;
+    const pimage = req.files["pimage"][0].filename;
+    const rdoc = req.files["rdoc"][0].filename;
+    let r = "";
+    if (name && email && pimage && rdoc) {
+      const doc = new applicantModel({
+        name,
+        email,
+        pimage,
+        rdoc,
+      });
+      r = await doc.save();
+      // console.log(r)
     }
-
-    static saveProfile=async (req,res)=>{
-        const {name, email}=req.body
-        const pimage = req.files['pimage'][0].filename
-        const rdoc = req.files['rdoc'][0].filename
-        let r=''
-        if(name && email && pimage && rdoc){
-            const doc = new applicantModel({
-                name, email, pimage, rdoc
-            })
-            r = await doc.save()
-            // console.log(r)
-        }
-        // r=r.name
-        // console.log(req.files)
-        // console.log(req.body)
-        const all = await applicantModel.find()
-        res.render("home.ejs",{r, all})
-    }
+    // r=r.name
+    // console.log(req.files)
+    // console.log(req.body)
+    const all = await applicantModel.find();
+    // res.render("home.ejs", { r, all });
+    res.send({r, all});
+  };
 }
 
-export default formcontroller
+export default formcontroller;
